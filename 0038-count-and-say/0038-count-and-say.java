50933746b63e95dba846f884cd3cb88d44df0a0c
shailesh pratap singh <142075865+shaileshpratapsingh132@users.noreply.github.com>
2026-09-01 02:14:46 +0530

    Time: 31 ms (5.45%), Space: 52.9 MB (5.38%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String countAndSay(int n) {
+        if (n==1){
+            return "1";
+        }
+        if(n==2){
+            return "11";
+        }
+        String s="11";
+        for(int i=3; i<=n; i++){
+            String t="";
+            s=s+'#';
+            int c=1;
+            for(int j=1; j<s.length(); j++){
+                if(s.charAt(j) !=s.charAt(j-1)){
+                    t=t+c;
+                    t=t+s.charAt(j-1);
+                    c=1;
+
+                }
+                else{
+                    c++;
+                }
+            }
+            s=t;
+        }
+        return s;
+    }
+}
