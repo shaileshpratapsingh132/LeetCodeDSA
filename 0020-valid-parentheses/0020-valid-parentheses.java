23a3c9277dc6379ece6f3058ee2ad31c23cea62c
shailesh pratap singh <142075865+shaileshpratapsingh132@users.noreply.github.com>
2026-09-18 22:37:18 +0530

    Time: 5 ms (13.06%), Space: 43 MB (92.8%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Solution {
+
+    public boolean isValid(String s) {
+
+        Stack<Character> st = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+
+            if (c == '(' || c == '{' || c == '[') {
+                st.push(c);
+            } 
+            else {
+
+                if (st.isEmpty()) {
+                    return false;
+                }
+
+                char t = st.pop();
+
+                if (c == ')' && t != '(') {
+                    return false;
+                }
+
+                if (c == '}' && t != '{') {
+                    return false;
+                }
+
+                if (c == ']' && t != '[') {
+                    return false;
+                }
+            }
+        }
+
+        return st.isEmpty();
+    }
+}
